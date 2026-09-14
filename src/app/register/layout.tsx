5e169eadf56b390703896056edbf955ce6377e72
Yash Saxena <yashcoding21@gmail.com>
2026-09-14 10:53:16 +0530

    feat: show global notices on login and register pages

---
+++ src/app/register/layout.tsx
@@ -0,0 +1,12 @@
+import { GlobalNotice } from "@/components/GlobalNotice"
+
+export default function RegisterLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="relative">
+      <div className="absolute top-0 left-0 w-full z-50 p-4 max-w-md mx-auto right-0">
+        <GlobalNotice />
+      </div>
+      {children}
+    </div>
+  )
+}

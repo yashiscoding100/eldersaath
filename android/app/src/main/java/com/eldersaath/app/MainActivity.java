f6a72c0e9b483499a6e3ec09eb16c7e8c0d464a5
Yash Saxena <yashcoding21@gmail.com>
2026-09-25 16:02:01 +0530

    feat: bypass android lock screen for alarms

--- android/app/src/main/java/com/eldersaath/app/MainActivity.java
+++ android/app/src/main/java/com/eldersaath/app/MainActivity.java
@@ -1,5 +1,24 @@
 package com.eldersaath.app;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.os.Build;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true);
+            setTurnScreenOn(true);
+        } else {
+            getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+            );
+        }
+    }
+}
